9f3facbb3730d41c69cd08bc9229a5c81ad5cf78
mhhwheat <1057251411@qq.com>
2017-06-23 00:14:24 +0800

    first commit

---
+++ src/com/wheat/kotlin/KotlinSyntax.kt
@@ -0,0 +1,13 @@
+package com.wheat.kotlin
+
+/**
+ * Created by Administrator on 2017/6/22.
+ */
+fun main(args: Array<String>) {
+    println("Hello World!")
+    println(sum(3,5))
+}
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
